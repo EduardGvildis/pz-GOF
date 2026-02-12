aef170bbfd963f3220d4ef4a4ebc8275202d4922
Eduard <edicgvildis@gmail.com>
2026-02-12 09:29:06 +0200

    all

--- src/behavioral/observer/example.ts
+++ src/behavioral/observer/example.ts
@@ -9,12 +9,12 @@ const station = new WeatherStation();
 const display1 = new ConsoleDisplay('Display A');
 const display2 = new ConsoleDisplay('Display B');
 const alarm = new Alarm(30); // спрацьовує при >= 30°C
-
+const alarm12 = new Alarm(-22);
 // Підписуємо спостерігачів
 station.register(display1);
 station.register(display2);
 station.register(alarm);
-
+station.register(alarm12)
 // Змінюємо стан — всі підписані будуть повідомлені
 station.setTemperature(25);
 station.setTemperature(31);
@@ -23,7 +23,7 @@ station.setTemperature(31);
 station.unregister(display2);
 
 // Лише Display A і Alarm отримають оновлення
-station.setTemperature(28);
+station.setTemperature(-22);
 
 // Коментар: без патерна довелося б жорстко викликати оновлення для кожного клієнта
 // З патерном — міцна слабка зв'язка, можна додавати/видаляти слухачів під час виконання
